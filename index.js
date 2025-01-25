9530bbcbb0bb97991bb92138c1c7f818d238e514
CLaireLise-dev <clairedemettre@gmail.com>
2025-01-25 23:00:37 +0100

    Début de logique bouton clavier

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const themeButton = document.querySelector(".mainButton:nth-child(3)");
 const body = document.querySelector("body");
 const url = "https://trouve-mot.fr/api/random";
 const input = document.querySelector("input");
+const keyboardButton = document.querySelectorAll(".keyboardButton");
+const keyboardContainer = document.querySelector(".keyboard");
+let guessWord ;
 
 if (localStorage.getItem("theme") === "sombre") {
   darkMode();
 }
+
+async function getWord() {
+  const request = await fetch(url, { method: "GET" });
+  if (!request.ok) {
+    alert("Il y a eu un problème");
+  } else {
+    let word = await request.json();
+    guessWord = word[0].name;
+    const wordLength = word[0].name.length;
+    console.log(wordLength);
+    const dashes = "-".repeat(wordLength).trim();
+    input.value = dashes;
+  }
+}
+getWord();
 
 themeButton.addEventListener("click", () => {
   if (body.classList.contains("darkTheme")) {
@@ -19,6 +37,7 @@ themeButton.addEventListener("click", () => {
   }
 });
 
+
 function darkMode() {
   body.classList.add("transition");
   setTimeout(() => body.classList.remove("transition"), 300);
@@ -28,25 +47,18 @@ function darkMode() {
 }
 
 const letters = "abcdefghijklmnopqrstuvwxyz".split("");
-const keyboardContainer = document.querySelector(".keyboard");
+
 letters.forEach((letter) => {
-  const button = document.createElement("button");
-  button.textContent = letter;
-  button.classList.add("keyboardButton");
-  keyboardContainer.appendChild(button);
+  const keyboardButton = document.createElement("button");
+  keyboardButton.textContent = letter;
+  keyboardButton.classList.add("keyboardButton");
+  keyboardButton.addEventListener ("click", (event) => {
+    const guessedLetter = event.target.textContent;
+    if (guessWord.includes(guessedLetter)){
+      alert("Bonne lettre")
+    }else{
+      alert("Mauvaise lettre")
+    }
+  })
+  keyboardContainer.appendChild(keyboardButton);
 });
-
-async function getWord() {
-  const request = await fetch(url, { method: "GET" });
-  if (!request.ok) {
-    alert("Il y a eu un problème");
-  } else {
-    let word = await request.json();
-    const wordLength = word[0].name.length;
-    console.log(wordLength);
-    const dashes = "-".repeat(wordLength).trim();
-    input.value = dashes;
-  }
-}
-
-getWord();
