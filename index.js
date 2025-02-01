9af44fe3ca72c8169af19ee05bdf2783e893200c
CLaireLise-dev <clairedemettre@gmail.com>
2025-02-01 15:39:58 +0100

    Logique proposer un mot et finalisation du projet entier

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Sélection des éléments du DOM
+
 const themeButton = document.querySelector(".mainButton:nth-child(3)");
 const body = document.querySelector("body");
 const url = "https://trouve-mot.fr/api/random";
@@ -9,10 +11,16 @@ const textRemainingAttempts = document.querySelector("#remainingAttempts");
 const progressBar = document.querySelector(".progress");
 const newGameButton = document.querySelector("#newGame");
 const hangmanImage = document.querySelector("#hangmanImage");
+const proposedWordButton = document.querySelector("#proposedWord");
+
+//Initialisation des variables globales
 
 let guessWord;
 let displayArray = [];
 let remainingAttempts = 10;
+
+//Initialisation de l'affichage
+
 progressBar.style.width = "100%";
 textRemainingAttempts.textContent = `Coups ${remainingAttempts}`;
 
@@ -20,37 +28,25 @@ if (localStorage.getItem("theme") === "sombre") {
   darkMode();
 }
 
+//Récupération du mot aléatoire via l'API
+
 async function getWord() {
-  const request = await fetch(url, { method: "GET" });
-  if (!request.ok) {
-    alert("Oups...il y a eu un problème");
-  } else {
-    let word = await request.json();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Erreur lors de la récupération du mot");
+
+    let word = await response.json();
     guessWord = word[0].name;
-    const wordLength = word[0].name.length;
-    console.log(wordLength);
-    displayArray = "-".repeat(wordLength).trim().split("");
+    displayArray = "-".repeat(guessWord.length).split("");
     input.value = displayArray.join("");
+  } catch (error) {
+    alert("Oups... il y a eu un problème !");
   }
 }
 
 getWord();
 
-themeButton.addEventListener("click", () => {
-  if (body.classList.contains("darkTheme")) {
-    body.classList.add("transition");
-    setTimeout(() => body.classList.remove("transition"), 300);
-    body.classList.remove("darkTheme");
-    themeButton.textContent = "Thème sombre";
-    localStorage.setItem("theme", "clair");
-  } else {
-    darkMode();
-  }
-});
-
-newGameButton.addEventListener("click", () => {
-  location.reload();
-});
+//Fonctions de la page
 
 function updateProgressBar() {
   remainingAttempts--;
@@ -88,6 +84,50 @@ function updateHangmanImage() {
   }
 }
 
+function checkFullWord() {
+  let proposedWord = prompt("Veuillez proposer un mot entier :")
+    .trim()
+    .toLowerCase();
+
+  if (!/^[a-zA-Z]+$/.test(proposedWord)) {
+    alert(
+      "Erreur : Veuillez entrer uniquement des lettres (sans chiffres ni symboles)."
+    );
+    return;
+  }
+
+  if (proposedWord === guessWord) {
+    alert("Bravo! Vous avez trouvé le mot!");
+  } else {
+    alert("Désolée, ce n'est pas le bon mot");
+    updateProgressBar();
+  }
+}
+
+//Gestion des évènements sur les boutons
+
+themeButton.addEventListener("click", () => {
+  if (body.classList.contains("darkTheme")) {
+    body.classList.add("transition");
+    setTimeout(() => body.classList.remove("transition"), 300);
+    body.classList.remove("darkTheme");
+    themeButton.textContent = "Thème sombre";
+    localStorage.setItem("theme", "clair");
+  } else {
+    darkMode();
+  }
+});
+
+newGameButton.addEventListener("click", () => {
+  location.reload();
+});
+
+proposedWordButton.addEventListener("click", () => {
+  checkFullWord();
+});
+
+// Gestion du clavier virtuel
+
 letters.forEach((letter) => {
   const keyboardButton = document.createElement("button");
   keyboardButton.textContent = letter;
@@ -106,7 +146,7 @@ letters.forEach((letter) => {
     }
 
     event.target.classList.add("clickedLetter");
-    console.log(event.target.classList);
+    event.target.disabled = true;
   });
   keyboardContainer.appendChild(keyboardButton);
 });
